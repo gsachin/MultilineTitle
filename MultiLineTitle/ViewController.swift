173b852fb5ab59d071aa330cadc0918316e78cda
Sachin <gupts4@gene.com>
2024-05-30 16:28:47 -0700

    Initial commit

--- MultiLineTitle/ViewController.swift
+++ MultiLineTitle/ViewController.swift
@@ -19,13 +19,16 @@ class ViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    var containerView :UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Create a container view
-        let containerView = UIView()
-        containerView.translatesAutoresizingMaskIntoConstraints = false
         containerView.addSubview(titleLabel)
         
         // Add constraints to label and container view
@@ -34,16 +37,24 @@ class ViewController: UIViewController {
             titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
             titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor),
             titleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
-//            containerView.widthAnchor.constraint(equalToConstant: 200)
         ])
         
         // Set the custom title view
         self.navigationItem.titleView = containerView
         
         // Schedule the update of the title
-        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             self.updateTitle()
+            self.adjustNavigationBarHeight(to:200)
         }
+    }
+    private func adjustNavigationBarHeight(to height: CGFloat) {
+        guard let navigationBar = self.navigationController?.navigationBar else { return }
+        
+        var frame = navigationBar.frame
+        frame.size.height = height
+        navigationBar.frame = frame
+        navigationBar.layoutIfNeeded()
     }
 
     @objc
@@ -61,133 +72,17 @@ class ViewController: UIViewController {
     }
 
     private func adjustNavigationBarHeight() {
-        if let navigationBar = self.navigationController?.navigationBar, let containerView = self.navigationItem.titleView {
-            let fittingSize = CGSize(width: containerView.frame.width, height: UIView.layoutFittingCompressedSize.height)
-            let newSize = containerView.systemLayoutSizeFitting(fittingSize)
-            
-            var frame = navigationBar.frame
-            frame.size.height = newSize.height + 20 // Add some padding
-            navigationBar.frame = frame
-        }
+        guard let navigationBar = self.navigationController?.navigationBar,
+              let containerView = self.navigationItem.titleView else { return }
+        
+        // Calculate the fitting size
+        let fittingSize = CGSize(width: containerView.bounds.width, height: UIView.layoutFittingCompressedSize.height)
+        let sizeThatFits = containerView.systemLayoutSizeFitting(fittingSize)
+        
+        // Adjust the navigation bar height
+        var frame = navigationBar.frame
+        frame.size.height = sizeThatFits.height + 20 // Add some padding
+        navigationBar.frame = frame
+        navigationBar.layoutIfNeeded()
     }
 }
-
-
-//import UIKit
-
-//class ViewController: UIViewController {
-//    var titleg:UILabel = {
-//        let label = UILabel()
-//        label.backgroundColor = .clear
-//        label.numberOfLines = 0
-//        label.font = UIFont.boldSystemFont(ofSize: 16.0)
-//        label.textAlignment = .center
-//        label.textColor = .black
-//        label.text = "This is a"
-//        label.lineBreakMode = .byWordWrapping
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-////        let label = UILabel()
-////        label.backgroundColor = .clear
-////        label.numberOfLines = 0
-////        label.font = UIFont.boldSystemFont(ofSize: 16.0)
-////        label.textAlignment = .center
-////        label.textColor = .black
-////        label.attributedText = NSAttributedString("This is a\nmultiline string for the navBar\nthird line")
-////        label.lineBreakMode = .byWordWrapping
-////        label.sizeToFit()
-////
-////        self.navigationItem.titleView = label
-//
-////        let label = UILabel()
-////               label.backgroundColor = .clear
-////               label.numberOfLines = 0
-////               label.font = UIFont.boldSystemFont(ofSize: 16.0)
-////               label.textAlignment = .center
-////               label.textColor = .black
-////               label.attributedText = NSAttributedString(string: "This is a\nmultiline string for the navBar\nthird line")
-////               label.lineBreakMode = .byWordWrapping
-////
-////               // Use Auto Layout to set the label's constraints
-////               label.translatesAutoresizingMaskIntoConstraints = false
-////
-////               let container = UIView()
-////               container.addSubview(label)
-////               container.translatesAutoresizingMaskIntoConstraints = false
-////
-////               NSLayoutConstraint.activate([
-////                   label.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-////                   label.trailingAnchor.constraint(equalTo: container.trailingAnchor),
-////                   label.topAnchor.constraint(equalTo: container.topAnchor),
-////                   label.bottomAnchor.constraint(equalTo: container.bottomAnchor)
-////               ])
-////
-////               self.navigationItem.titleView = container
-////        let label = UILabel()
-////        label.backgroundColor = .clear
-////        label.numberOfLines = 0
-////        label.font = UIFont.boldSystemFont(ofSize: 16.0)
-////        label.textAlignment = .center
-////        label.textColor = .black
-////        label.attributedText = NSAttributedString(string: "This is a\nmultiline string for the navBar\nthird line\nfourth line\nfifth line")
-////        label.lineBreakMode = .byWordWrapping
-////
-////        // Use Auto Layout to set the label's constraints
-////        label.translatesAutoresizingMaskIntoConstraints = false
-////
-////        let container = UIView()
-////        container.addSubview(label)
-////        container.translatesAutoresizingMaskIntoConstraints = false
-////
-////        NSLayoutConstraint.activate([
-////            label.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-////            label.trailingAnchor.constraint(equalTo: container.trailingAnchor),
-////            label.topAnchor.constraint(equalTo: container.topAnchor),
-////            label.bottomAnchor.constraint(equalTo: container.bottomAnchor),
-////            //container.widthAnchor.constraint(equalToConstant: 400), // Adjust width as needed
-////            container.heightAnchor.constraint(lessThanOrEqualToConstant: 400) // Maximum height constraint
-////        ])
-////
-////        self.navigationItem.titleView = container
-//        // Create the label
-//
-//
-//                // Create a container view
-//                let containerView = UIView()
-//                containerView.translatesAutoresizingMaskIntoConstraints = false
-//                containerView.addSubview(titleg)
-//
-//                // Add constraints to label and container view
-//                NSLayoutConstraint.activate([
-//                    titleg.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-//                    titleg.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-//                    titleg.topAnchor.constraint(equalTo: containerView.topAnchor),
-//                    titleg.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
-////                    containerView.widthAnchor.constraint(equalToConstant: 200)
-//                ])
-//
-//                // Adjust the height constraint of the container view
-//                containerView.heightAnchor.constraint(lessThanOrEqualToConstant: 200).isActive = true
-//
-//                // Set the custom title view
-//                self.navigationItem.titleView = containerView
-//        let dq = DispatchQueue(label: "background")
-//        dq.perform(#selector(yourMethod), with: nil, afterDelay: 10)
-//
-//        //self.perform(#selector(yourMethod), on: .main, wait: false, modes: [.default])
-//    }
-//    @objc
-//    func yourMethod(){
-//        DispatchQueue.main.async {
-//            self.titleg.text = "This is a\nmultiline string for the navBar third line\nfourth line\nfifth line"
-//        }
-//
-//
-//    }
-//
-//}
-
